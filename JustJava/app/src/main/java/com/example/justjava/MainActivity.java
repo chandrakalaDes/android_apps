d0a783d018981901424c17cc017f13bf8087d996
chandrakalaDes <51953236+chandrakalaDes@users.noreply.github.com>
2019-07-02 13:57:23 +0530

    just java version 1

--- JustJava/app/src/main/java/com/example/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/justjava/MainActivity.java
@@ -6,8 +6,10 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.NumberFormat;
 
+public class MainActivity extends AppCompatActivity {
+    int quantity=2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,14 +20,35 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(1);
+
+        displayPrice(quantity * 5);
+        display(quantity);
+    }
+    public void increase(View view) {
+
+        quantity=quantity+1;
+        display(quantity);
+    }
+    public void decrement(View view) {
+
+        quantity=quantity-1;
+        display(quantity);
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
     private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        TextView quantityTextView = findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
